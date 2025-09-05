28a64ab1b04bac723d8f60fea16b068d4ecc5873
BirhaDaniel0374 <naolbirhanunaol@gmail.com>
2025-09-05 23:32:51 +0300

    Fix lightningcss build error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   experimental: {
+    optimizeCss: false, // disables lightningcss
+  },
 };
 
 export default nextConfig;
